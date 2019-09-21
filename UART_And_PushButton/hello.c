a7c9a98e128754b4b7d2814e8cdfd190fe4f32c5
abdulrahmantablya <abdulrahmantablya@gmail.com>
2019-09-21 21:25:06 +0200

    UART_And_PushButton_TX

---
+++ UART_And_PushButton/hello.c
@@ -0,0 +1,224 @@
+//*****************************************************************************
+//
+// hello.c - Simple hello world example.
+//
+// Copyright (c) 2012-2017 Texas Instruments Incorporated.  All rights reserved.
+// Software License Agreement
+// 
+// Texas Instruments (TI) is supplying this software for use solely and
+// exclusively on TI's microcontroller products. The software is owned by
+// TI and/or its suppliers, and is protected under applicable copyright
+// laws. You may not combine this software with "viral" open-source
+// software in order to form a larger program.
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
+// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
+// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
+// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
+// DAMAGES, FOR ANY REASON WHATSOEVER.
+// 
+// This is part of revision 2.1.4.178 of the EK-TM4C123GXL Firmware Package.
+//
+//*****************************************************************************
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+
+
+
+#include "inc/hw_gpio.h"
+
+//#include "inc/hw_timer.h"
+//#include "driverlib/timer.h"
+
+#include "inc/hw_uart.h"
+
+#include "inc/hw_ints.h"
+#include "driverlib/interrupt.h"
+
+//*****************************************************************************
+//
+//! \addtogroup example_list
+//! <h1>Hello World (hello)</h1>
+//!
+//! A very simple ``hello world'' example.  It simply displays ``Hello World!''
+//! on the UART and is a starting point for more complicated applications.
+//!
+//! UART0, connected to the Virtual Serial Port and running at
+//! 115,200, 8-N-1, is used to display messages from this application.
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+// The error routine that is called if the driver library encounters an error.
+//
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+}
+#endif
+
+//*****************************************************************************
+//
+// Configure the UART and its pins.  This must be called before UARTprintf().
+//
+//*****************************************************************************
+void
+ConfigureUART(void)
+{
+    //
+    // Enable the GPIO Peripheral used by the UART.
+    //
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+
+    //
+    // Enable UART0
+    //
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+
+    //
+    // Configure GPIO Pins for UART mode.
+    //
+    ROM_GPIOPinConfigure(GPIO_PB0_U1RX);
+    ROM_GPIOPinConfigure(GPIO_PB1_U1TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    //
+    // Use the internal 16MHz oscillator as the UART clock source.
+    //
+    UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC);
+
+    //
+    // Initialize the UART for console I/O.
+    //
+    UARTStdioConfig(1, 115200, 16000000);
+}
+
+//*****************************************************************************
+//
+// Print "Hello World!" to the UART on the evaluation board.
+//
+//*****************************************************************************
+
+
+//******************************************************************************************
+
+
+
+int
+main(void)
+{
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+        while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF));
+
+        HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+        HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;
+        HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
+
+        GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+        GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4);
+        GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+
+//        GPIOIntTypeSet(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4, GPIO_RISING_EDGE);
+//        GPIOIntRegister(GPIO_PORTF_BASE, ButtonHandler);
+//        GPIOIntEnable(GPIO_PORTF_BASE, GPIO_INT_PIN_0 | GPIO_INT_PIN_4);
+
+    //volatile uint32_t ui32Loop;
+
+    //
+    // Enable lazy stacking for interrupt handlers.  This allows floating-point
+    // instructions to be used within interrupt handlers, but at the expense of
+    // extra stack usage.
+    //
+    ROM_FPULazyStackingEnable();
+
+    //
+    // Set the clocking to run directly from the crystal.
+    //
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ |
+                       SYSCTL_OSC_MAIN);
+
+    //
+    // Enable the GPIO port that is used for the on-board LED.
+    //
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    //
+    // Enable the GPIO pins for the LED (PF2 & PF3).
+    //
+    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+
+    //
+    // Initialize the UART.
+    //
+    //ConfigureUART();
+    ConfigureUART();
+
+    //
+    // Hello!
+    //
+
+    //
+    // We are finished.  Hang around doing nothing.
+    //
+
+    while(1)
+    {
+
+        if ( GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4) == 0 )
+        {
+            while( GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4) == 0 );
+
+            UARTCharPut(UART1_BASE,1);
+
+
+
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+
+                   //
+                   // Delay for a bit.
+                   //
+                   SysCtlDelay(SysCtlClockGet() / 10 / 3);
+
+                   //
+                   // Turn off the BLUE LED.
+                   //
+                   GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+
+                   //
+                   // Delay for a bit.
+                   //
+                   SysCtlDelay(SysCtlClockGet() / 10 / 3);
+
+        }
+        else
+        {
+
+        }
+
+
+
+
+
+
+        //
+        // Turn on the BLUE LED.
+        //
+
+    }
+}
